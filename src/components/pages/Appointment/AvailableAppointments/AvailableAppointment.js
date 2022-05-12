314bc31c82e6b9de7cc2f079df075876ed226f77
tanveer <mdtanviralamht731@gmail.com>
2022-05-12 10:17:03 +0600

    conditional rendaring added on  availableAppointment component

--- src/components/pages/Appointment/AvailableAppointments/AvailableAppointment.js
+++ src/components/pages/Appointment/AvailableAppointments/AvailableAppointment.js
@@ -5,8 +5,20 @@ const AvailableAppointment = ({ data }) => {
   return (
     <div class="card lg:max-w-lg hover:border  shadow-lg text-neutral-content">
       <div class="card-body items-center text-center">
-        <h2 class="card-title text-secondary font-semibold my-6">{name}</h2>
-        <p className="text-sm my-6 text-neutral">10 SPACES AVAILABLE</p>
+        <h2 class="card-title text-secondary font-semibold mb-[10px] p-0">
+          {name}
+        </h2>
+
+        <p className="text-center text-sm text-accent mb-[10px] p-0">
+          {slots.length > 0 ? (
+            <span>{slots[0]}</span>
+          ) : (
+            <span className="text-red-500">Try Another Day</span>
+          )}
+        </p>
+        <p className="text-sm mb-[15px] p-0 text-neutral">
+          {slots.length} {slots.length < 1 ? "SPACE" : "SPACES"} AVAILABLE
+        </p>
         <div class="card-actions justify-end">
           <button class="btn btn-primary uppercase text-base-100 bg-gradient-to-r from-secondary to-primary">
             book appointment
